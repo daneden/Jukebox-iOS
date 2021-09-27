9b1100f22c5fec63b5d7a781d5a65783b4c65f7c
Daniel Eden <dan.eden@me.com>
2021-09-27 10:02:28 +0100

    Initial Commit

---
+++ Jukebox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jukebox
+//
+//  Created by Daniel Eden on 27/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jukebox/JukeboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  JukeboxApp.swift
+//  Jukebox
+//
+//  Created by Daniel Eden on 27/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct JukeboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
